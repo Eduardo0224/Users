f095ac8e36d0bf532428f4b2917bef9984949d78
Eduardo Andrade <eduardrade0224@gmail.com>
2020-06-27 08:35:43 -0500

    Initial Commit

---
+++ Users/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Users
+//
+//  Created by Eduardo Andrade on 27/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
